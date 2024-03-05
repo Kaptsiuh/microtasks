4c2b090659871c0d35b4b2171cffaba1d8add458
Dima Berry <dimaberry1@gmail.com>
2024-03-05 20:18:21 +0100

    complete task input

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,41 @@
 import React, { useState } from "react";
 import "./App.css";
-import { NewComponent } from "./components/NewComponent";
-import { Button } from "./components/Button";
-import { NewComponent2 } from "./components/NewComponent2";
-
-type FilterType = "all" | "Dollars" | "RUBLS";
+// import { FullInput } from "./components/FullInput";
+import { Input } from "./components/Input/Input";
+import { Button } from "./components/Input/Button";
+// import { NewComponent } from "./components/NewComponent";
+// import { Button } from "./components/Button";
+// import { NewComponent2 } from "./components/NewComponent2";
 
 function App() {
-  const [money, setMoney] = useState([
-    { banknots: "Dollars", value: 100, number: " a1234567890" },
-    { banknots: "Dollars", value: 50, number: " z1234567890" },
-    { banknots: "RUBLS", value: 100, number: " w1234567890" },
-    { banknots: "Dollars", value: 100, number: " e1234567890" },
-    { banknots: "Dollars", value: 50, number: " c1234567890" },
-    { banknots: "RUBLS", value: 100, number: " r1234567890" },
-    { banknots: "Dollars", value: 50, number: " x1234567890" },
-    { banknots: "RUBLS", value: 50, number: " v1234567890" },
+  const [message, setMessage] = useState([
+    { message: "message1" },
+    { message: "message2" },
+    { message: "message3" },
+    { message: "message4" },
+    { message: "message5" },
   ]);
-  const [filter, setFilter] = useState("all");
 
-  let currentMoney = money;
-  if (filter === "Dollars") {
-    currentMoney = money.filter(
-      (filteredMoney) => filteredMoney.banknots === "Dollars"
-    );
-  }
-  if (filter === "RUBLS") {
-    currentMoney = money.filter(
-      (filteredMoney) => filteredMoney.banknots === "RUBLS"
-    );
-  }
+  const [title, setTitle] = useState("");
 
-  const onClickFilterHandler = (nameButton: FilterType) => {
-    setFilter(nameButton);
+  const addMessage = (title: string) => {
+    setMessage([{ message: title }, ...message]);
+  };
+
+  const callBackButtonHandler = () => {
+    addMessage(title);
+    setTitle("");
   };
 
   return (
-    <>
-      {/* <NewComponent2 onClick={onClickFilterHandler} currentMoney={currentMoney} /> */}
-      <ul>
-        {currentMoney.map((objFromMoneyArr, index) => {
-          return (
-            <li key={index}>
-              <span>{objFromMoneyArr.banknots}</span>
-              <span>{objFromMoneyArr.value}</span>
-              <span>{objFromMoneyArr.number}</span>
-            </li>
-          );
-        })}
-      </ul>
-      <button onClick={() => onClickFilterHandler("all")}>all</button>
-      <button onClick={() => onClickFilterHandler("RUBLS")}>rubl</button>
-      <button onClick={() => onClickFilterHandler("Dollars")}>dollar</button>
-    </>
+    <div className="App">
+      {/* <FullInput addMessage={addMessage} /> */}
+      <Input setTitle={setTitle} title={title} />
+      <Button name={"+"} callBack={callBackButtonHandler} />
+      {message.map((el, index) => {
+        return <div key={index}>{el.message}</div>;
+      })}
+    </div>
   );
 }
 
---
+++ src/components/Input/Button.tsx
@@ -0,0 +1,12 @@
+type ButtonProps = {
+  name: string;
+  callBack: () => void;
+};
+
+export const Button = (props: ButtonProps) => {
+  const onClickButtonHandler = () => {
+    props.callBack();
+  };
+
+  return <button onClick={onClickButtonHandler}>{props.name}</button>;
+};
